cab75a2ea25d968bfb5d28e9ea03397349d8f3e8
federico_viganò <fede_viga@hotmail.it>
2024-04-10 17:53:33 +0200

    aggiunti stili - fine esercizio

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,9 +8,10 @@
         <h2 class="mb-4">
             Treni in partenza oggi:
         </h2>
-        <table class="table">
+        <table class="table table-dark">
             <thead>
               <tr>
+                <th scope="col">#</th>
                 <th scope="col">Azienda</th>
                 <th scope="col">Stazione di Partenza</th>
                 <th scope="col">Stazione di Arrivo</th>
@@ -20,16 +21,29 @@
                 <th scope="col">Cancellato</th>
               </tr>
             </thead>
-            <tbody>
-                @foreach($trains as $train)
+            <tbody class="table-group-divider">
+                @foreach($trains as $key => $train)
                 <tr>
+                  <th scope="row">{{$key + 1}}</th>
                   <td>{{$train->azienda}}</td>
                   <td>{{$train->stazione_partenza}}</td>
                   <td>{{$train->stazione_arrivo}}</td>
                   <td>{{$train->orario_partenza}}</td>
                   <td>{{$train->codice_treno}}</td>
-                  <td>{{$train->in_orario}}</td>
-                  <td>{{$train->cancellato}}</td>
+                  <td>
+                    @if($train->in_orario == true)
+                    Si
+                    @else
+                    No
+                    @endif
+                  </td>
+                  <td>
+                    @if($train->cancellato == true)
+                    Si
+                    @else
+                    No
+                    @endif
+                  </td>
                 </tr>
                 @endforeach
             </tbody>
