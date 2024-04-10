96160617c85daff3867b8a504919c29e9ba9760a
federico_viganò <fede_viga@hotmail.it>
2024-04-10 15:32:51 +0200

    creata migration tabella trains

---
+++ database/migrations/2024_04_10_130329_create_trains_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('azienda', 50);
+            $table->string('stazione_partenza', 100);
+            $table->string('stazione_arrivo', 100);
+            $table->date('data');
+            $table->time('orario_partenza');
+            $table->time('orario_arrivo');
+            $table->char('codice_treno',  10);
+            $table->smallInteger('numero_carrozze');
+            $table->boolean('in_orario');
+            $table->boolean('cancellato');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trains');
+    }
+};
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,11 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Titolo</title>
+    <title>Laravel Migration Seeder</title>
 
     @vite('resources/js/app.js')
 </head>
-<body>
+<body class="bg-dark">
+
+    <h1 class="text-white text-center py-5">
+        Laravel Migration Seeder
+    </h1>
 
 @yield('content')
     
